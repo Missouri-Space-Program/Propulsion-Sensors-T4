9053c732883ff610559432da912717429faf85a4
Ryan Milewski <rmilewski44@gmail.com>
2024-02-03 15:55:41 -0600

    Working Load measurement

--- MSPPropulsionTest.py
+++ MSPPropulsionTest.py
@@ -33,7 +33,7 @@ loadAdjVoltage = 0.0
 load = 0.0
 while(True):
     pressVoltage = ljm.eReadName(handle,"AIN0")
-    loadVoltage = ljm.eReadName(handle,"AIN5");
+    loadVoltage = ljm.eReadName(handle,"AIN5")
     pressAdjVoltage = pressVoltage - TRANSDUCERMINVOLTAGE
     pressure = pressAdjVoltage * TRANSDUCERSCALINGFACTOR
     #1.25 and 201 taken from dip switches active on the LJ-Tick-InAmp. For more info see https://labjack.com/pages/support?doc=/datasheets/accessories/ljtick-inamp-datasheet/
@@ -41,8 +41,11 @@ while(True):
     #Load = RatedLoad *(VAIN- Voffset)/ (gain * Sensitivity * Vexc) in kilos
     #our load cell's senitivity is 2 mV/V
     #need to figure out Vexc, which is the excitition of the load cell circuit
-    Vexc = 0.0
-    load = 500 * (loadVoltage - 1.25) / (201 * 2 * Vexc)
+    Vexc = 2.5
+    #load = 500 * (loadVoltage - 1.25) / (201 * 2 * Vexc)
+    load = 500 * loadAdjVoltage / (2 * 2.5)
+    #found by calculating the slope and intercept between two known weights and the adjusted voltage
+    calibratedLoad = 92290.3226 * loadAdjVoltage - 2.5356
     print("  %d  Raw Voltage - %f, Adjusted Voltage : %f, Pressure (PSI): %f" % (counter, pressVoltage, pressAdjVoltage, pressure))
-    print("  %d  Raw Voltage - %f, Adjusted Voltage : %f, Load (kg) : %f " % (counter, loadVoltage, loadAdjVoltage, load))
+    print("  %d  Raw Voltage - %f, Adjusted Voltage : %f, Load (kg) : %f " % (counter, loadVoltage, loadAdjVoltage, calibratedLoad))
     counter += 1
