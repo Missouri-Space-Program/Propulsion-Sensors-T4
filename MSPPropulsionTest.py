1ccf05429815541e7cc6777809960fbffe1f1c79
Ryan Milewski <rmilewski44@gmail.com>
2024-02-08 17:58:55 -0600

    Get correct scaling factor of load cell, take avgs

--- MSPPropulsionTest.py
+++ MSPPropulsionTest.py
@@ -3,6 +3,7 @@ TRANSDUCERMINVOLTAGE = 0.5
 TRANSDUCERMAXVOLTAGE = 4.5
 TRANSDUCERMAXPRESSURE = 1600 #In PSI
 TRANSDUCERSCALINGFACTOR = TRANSDUCERMAXPRESSURE / (TRANSDUCERMAXVOLTAGE-TRANSDUCERMINVOLTAGE)
+SAMPLES = 5
 # Open first found LabJack
 handle = ljm.openS("ANY","ANY","192.168.20.3")
 
@@ -27,25 +28,40 @@ pressAdjVoltage = 0.0
 counter = 0
 
 #AIN5 (Load Cell)
-
+aNames = ["AIN5_RESOLUTION_INDEX", "AIN5_SETTLING_US"]
+aValues = [0, 0]
+numFrames = len(aNames)
+ljm.eWriteNames(handle,numFrames,aNames,aValues)
 loadVoltage = 0.0
 loadAdjVoltage = 0.0
 load = 0.0
 while(True):
-    pressVoltage = ljm.eReadName(handle,"AIN0")
-    loadVoltage = ljm.eReadName(handle,"AIN5")
+    #pressVoltage = ljm.eReadName(handle,"AIN0")
+    #loadVoltage = ljm.eReadName(handle,"AIN5")
+    avgCounter = 0
+    pressVoltageSum = 0.0
+    loadVoltageSum = 0.0
+    #taking average of specified sample number
+    while(avgCounter < SAMPLES):
+      pressVoltageSum += ljm.eReadName(handle,"AIN0")
+      loadVoltageSum += ljm.eReadName(handle,"AIN5") 
+      avgCounter += 1
+
+    pressVoltage = pressVoltageSum / SAMPLES
+    loadVoltage = loadVoltageSum / SAMPLES
     pressAdjVoltage = pressVoltage - TRANSDUCERMINVOLTAGE
     pressure = pressAdjVoltage * TRANSDUCERSCALINGFACTOR
     #1.25 and 201 taken from dip switches active on the LJ-Tick-InAmp. For more info see https://labjack.com/pages/support?doc=/datasheets/accessories/ljtick-inamp-datasheet/
     loadAdjVoltage = (loadVoltage - 1.25) / 201
     #Load = RatedLoad *(VAIN- Voffset)/ (gain * Sensitivity * Vexc) in kilos
     #our load cell's senitivity is 2 mV/V
-    #need to figure out Vexc, which is the excitition of the load cell circuit
-    Vexc = 2.5
+    #Excition voltage available on Input 6.
+    Vexc = ljm.eReadName(handle, "AIN6")
+    print(Vexc)
     #load = 500 * (loadVoltage - 1.25) / (201 * 2 * Vexc)
     load = 500 * loadAdjVoltage / (2 * 2.5)
     #found by calculating the slope and intercept between two known weights and the adjusted voltage
-    calibratedLoad = 92290.3226 * loadAdjVoltage - 2.5356
+    calibratedLoad = 100387.5 * loadAdjVoltage - 3.8069375
     print("  %d  Raw Voltage - %f, Adjusted Voltage : %f, Pressure (PSI): %f" % (counter, pressVoltage, pressAdjVoltage, pressure))
     print("  %d  Raw Voltage - %f, Adjusted Voltage : %f, Load (kg) : %f " % (counter, loadVoltage, loadAdjVoltage, calibratedLoad))
     counter += 1
